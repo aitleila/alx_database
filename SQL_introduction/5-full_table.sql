517622618dc57554c56585f1cec372dbd9f64e04
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-14 17:17:19 +0100

    l

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,9 +1,7 @@
 -- Script that prints the full description of the table first_table from the database hbtn_0c_0
 
-
 Script TABLE AS CREATE first_table;
 
-SELECT CREATE TABLE
+SELECT* 
 FROM INFORMATION_SHEMA.TABLES
 WHERE TABLE_NAME = 'first_table' AND TABLE_SHEMA = 'hbtn_0c_0';
-
