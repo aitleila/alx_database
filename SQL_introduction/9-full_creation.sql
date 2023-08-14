8c02c905c10f0914876377de46d32517dc9e2322
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-14 19:09:36 +0100

    L

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,11 @@
 -- Script that creates a table second_table in the database hbtn_0c_0
 
-CREATE IF NOT EXISTS TABLE second_table(
+CREATE IF NOT EXISTS second_table(
     id INT,
     name VARCHAR(256),
     score INT
     );
-        INSERT INTO TABLE second_table (id, name, score)
+        INSERT INTO second_table (id, name, score)
         VALUES (
             (1, 'John', 10),
             (2, 'Alex', 3),
