ce18ed4d262e5eed916360eb168db31901aa871d
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-14 18:33:35 +0100

    h

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,6 +1,3 @@
 -- Script that displays the number of records with id = 89 in the table first_table:
 
-SELECT*
-COUNT id
-FROM first_table
-WHERE 'id = 89';
+SELECT COUNT ('id=89') FROM first_table;
