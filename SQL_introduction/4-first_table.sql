f2dcbade9304797faf803b62b7ffdd49672b1379
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-14 03:48:03 +0100

    j

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- Script that creates a table called first_table in the current database:
+
+CREATE TABLE IF NOT EXISTS first_table(
+    first_tableID int,
+    first_tableName VARCHAR(256)
+);
+
+
