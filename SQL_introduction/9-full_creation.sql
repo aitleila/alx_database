785a5cdb5cbbcce42ef62bf03a55b715501a39f6
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-14 18:53:34 +0100

    k

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -5,9 +5,10 @@ CREATE IF NOT EXISTS TABLE second_table(
     name VARCHAR(256),
     score INT
     );
-INSERT INTO second_table VALUES (
-    id = 1, name = “John”, score = 10,
-    id = 2, name = “Alex”, score = 3,
-    id = 3, name = “Bob”, score = 14,
-    id = 4, name = “George”, score = 8
-    );
+        INSERT INTO second_table(id, name,score)
+        VALUES (
+            id = 1, name = “John”, score = 10,
+            id = 2, name = “Alex”, score = 3,
+            id = 3, name = “Bob”, score = 14,
+            id = 4, name = “George”, score = 8
+            );
