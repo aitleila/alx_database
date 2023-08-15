e35ff47cf603590af23144a46b1a1cbb7a437002
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-16 00:50:01 +0100

    H

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,5 @@
 -- Script that lists all privileges of users:
 
-SHOW GRANTS FOR 
-'user_0d_1'@`localhost`;
-'user_0d_2'@`localhost`;
+SHOW GRANTS FOR 'user_0d_1'@`localhost`;
+SHOW GRANTS FOR 'user_0d_2'@`localhost`;
 
