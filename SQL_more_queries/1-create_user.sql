370a9a10560fedb22ba5f7b52a18601c6793fa50
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-16 00:56:09 +0100

    G

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,5 @@
 -- Create the user user_0d_1 with all privileges:
-CREATE DATABASE
-    IF NOT EXISTS 'test_database';
+
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
@@ -8,3 +7,4 @@ GRANT ALL PRIVILEGES
    ON *.*
    TO 'user_0d_1'@'localhost'
    IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
