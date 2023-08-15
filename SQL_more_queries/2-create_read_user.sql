84aab9d5c01b113834f899e618863f469b42705f
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-16 00:23:33 +0100

    J

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -7,10 +7,8 @@ CREATE USER
     IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT 
-    ON 'localhost'
+    ON 'localhost'.*
     TO 'user_0d_2'
     IDENTIFIED BY 'user_0d_2_pwd';
 
 FLUSH PRIVILEGES;
-
-
