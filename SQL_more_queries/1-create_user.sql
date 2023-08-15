2cd2d0a4fdd47fb0a09ec3afba3d78ea277759ee
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-16 00:53:46 +0100

    h

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,6 @@
 -- Create the user user_0d_1 with all privileges:
+CREATE DATABASE
+    IF NOT EXISTS 'test_database';
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
