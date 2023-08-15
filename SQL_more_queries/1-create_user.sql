44a05e83a6127a970d40a755d9d7cda630d76b03
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-15 22:23:41 +0100

    f

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,3 +3,5 @@
 CREATE IF NOT EXISTS USER 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
 
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+exit
