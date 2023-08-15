142ce3926e3308da5ab4bd41bc146eb56f7fc471
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-15 22:11:59 +0100

    h

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Script that creates the MySQL server user user_0d_1:
+
+CREATE IF NOT EXISTS USER 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
