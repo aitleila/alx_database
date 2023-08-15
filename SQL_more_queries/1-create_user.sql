75d6f183c97c4d6822273fb67a5075ea2e6db25f
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-16 00:44:27 +0100

    H

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,13 +1,9 @@
--- Script that creates the MySQL server user user_0d_1:
-
-CREATE USER 
-    IF NOT EXISTS 'user_0d_1'@'localhost' 
+-- Creates the user user_0d_1 with all privileges.
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
-
-GRANT ALL PRIVILEGES 
-    ON *.* 
-    TO 'user_0d_1'@'localhost' 
-    IDENTIFIED BY 'user_0d_1_pwd' 
-    WITH GRANT OPTION;
-
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
