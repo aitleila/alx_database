9359f960b07de2aaeff7085851d195791798d88d
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-16 02:51:05 +0100

    H

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -4,7 +4,7 @@ CREATE database
     IF NOT EXISTS 'hbtn_0d_usa';
 
 CREATE TABLE
-    IF NOT EXISTS 'states'@'hbtn_0d_usa'(
+    IF NOT EXISTS 'states'.'hbtn_0d_usa'(
         'id' INT PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL,
         'name' VARCHAR(256) NOT NULL
     );
