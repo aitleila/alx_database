9d18113b67b743cd0d4b3c2a746def2fa17a4364
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-16 01:38:19 +0100

    K

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,9 +1,9 @@
 --  script that creates the table force_name:
 
 CREATE TABLE
-    IF NOT EXISTS 'force_name'(id, name)(
-        INT,
-        VARCHAR(256) NOT NULL
+    IF NOT EXISTS 'force_name'(
+        'id' INT,
+        'name' VARCHAR(256) NOT NULL
     );
     
 
