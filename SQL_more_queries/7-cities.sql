7346915eebe3088b7c3fc8e354ccb4ff0cb02086
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-17 19:45:28 +0100

    j

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -4,11 +4,11 @@ CREATE DATABASE
     IF NOT EXISTS hbtn_0d_usa;
 
 --Usage of the database:
-USE DATABASE hbtn_0d_usa;
+USE hbtn_0d_usa;
 
 --Create the table
 CREATE TABLE 
-    IF NOT EXISTS cities@(
+    IF NOT EXISTS cities(
         id INT PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL, 
         state_id INT NOT NULL AND FOREIGN KEY TO TABLE state_id,
         name VARCHAR(256) NOT NULL
