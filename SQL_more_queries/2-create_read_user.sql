ef3ba5a799c9c67fbafe9841051fdebb345fe1cc
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-17 18:28:42 +0100

    j

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- Script that creates the database hbtn_0d_2 and the user user_0d_2:
 
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 --Create the user and update the password:
 
@@ -8,4 +8,4 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant privileges:
 
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
