bd0f2fc50693d4db2831695c31a4bbf9f1f48ecf
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-17 18:23:51 +0100

    k

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,11 @@
 
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 
+--Create the user and update the password:
+
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant privileges:
 
 GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
