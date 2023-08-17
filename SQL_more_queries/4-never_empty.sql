36e8ae9343e89aa7950f93360890d78642a33890
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-17 18:42:00 +0100

    j

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,8 +1,4 @@
 -- Script that creates the table id_not_null:
 
-CREATE TABLE 
-    IF NOT EXISTS 'id_not_null'(
-        'id' INT VALUE 1,
-        'name' VARCHAR(256)
-    );
+CREATE TABLE IF NOT EXISTS id_not_null (id INT VALUE 1,name VARCHAR(256));
     
