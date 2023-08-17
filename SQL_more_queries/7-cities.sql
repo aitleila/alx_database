b629a8d4a2b0a23df2e22177746069ebdb4f1c6a
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-17 19:40:58 +0100

    h

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,10 +1,15 @@
 -- Script that creates the database hbtn_0d_usa and the table cities:
 
 CREATE DATABASE 
-    IF NOT EXISTS 'hbtn_0d_usa';
+    IF NOT EXISTS hbtn_0d_usa;
+
+--Usage of the database:
+USE DATABASE hbtn_0d_usa;
+
+--Create the table
 CREATE TABLE 
-    IF NOT EXISTS 'cities'@'hbtn_0d_usa'(
-        'id' INT PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL, 
-        'state_id' INT NOT NULL AND FOREIGN KEY TO TABLE 'state_id',
-        'name' VARCHAR(256) NOT NULL
+    IF NOT EXISTS cities@(
+        id INT PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL, 
+        state_id INT NOT NULL AND FOREIGN KEY TO TABLE state_id,
+        name VARCHAR(256) NOT NULL
     );
