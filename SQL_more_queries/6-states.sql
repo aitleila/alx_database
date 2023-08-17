65b2df64ba0b3f1e3734c89fa0f3a1b13e1e3136
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-17 19:34:49 +0100

    n

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -4,7 +4,7 @@ CREATE database IF NOT EXISTS hbtn_0d_usa;
 -- Usage of the database:
 USE hbtn_0d_usa;
 -- Create the table:
-CREATE TABLE IF NOT EXISTS 'states'(
+CREATE TABLE IF NOT EXISTS states(
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     name VARCHAR(256) NOT NULL
     );
