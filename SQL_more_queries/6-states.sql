0ed13e93526e4c12763ca7d67912f5d162a54744
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-17 18:57:25 +0100

    k

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,11 +1,11 @@
 -- Script that creates the database hbtn_0d_usa and the table states:
 
-CREATE database
-    IF NOT EXISTS 'hbtn_0d_usa';
-
-CREATE TABLE
-    IF NOT EXISTS 'states.hbtn_0d_usa'(
-        'id' INT PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL,
-        'name' VARCHAR(256) NOT NULL
+CREATE database IF NOT EXISTS hbtn_0d_usa;
+-- Usage of the database:
+USE hbtn_0d_usa;
+-- Create the table:
+CREATE TABLE IF NOT EXISTS 'states'(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(256) NOT NULL
     );
 
