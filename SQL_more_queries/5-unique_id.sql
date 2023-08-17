457920ab9220b8d7e271b0041348aef6ee194908
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-17 18:45:13 +0100

    K

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,8 +1,4 @@
 -- Script that creates the table unique_id:
 
-CREATE TABLE 
-    IF NOT EXISTS 'unique_id'(
-        'id' INT DEFAULT IDENTITY 1 UNIQUE,
-        'name' VARCHAR(256)
-    );
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT IDENTITY 1 UNIQUE, name VARCHAR(256));
 
