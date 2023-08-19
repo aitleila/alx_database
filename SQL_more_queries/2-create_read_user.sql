7f066b05345bf24d32ef7b44f084772a643cfef6
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-19 17:19:18 +0100

    g

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,13 +2,16 @@
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
-USE hbtn_0d_2;
+USE my_sql;
 
---Create the user and update the password:
+--Create the user and set the password:
 
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+SET PASSWORD FOR 'user_0d_2'@'localhost'= 'user_0d_2_pwd';
 
 -- Grant privileges:
 
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
 FLUSH PRIVILEGES;
